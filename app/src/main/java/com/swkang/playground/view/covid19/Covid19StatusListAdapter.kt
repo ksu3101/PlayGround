81ed3d2e53c6cd9feeea208e4b5cf5d4420bbbf8
beemo <burkdog@naver.com>
2020-12-17 00:37:32 +0900

    [수정] ListAdapter와 ViewHolder수정

--- app/src/main/java/com/swkang/playground/view/covid19/Covid19StatusListAdapter.kt
+++ app/src/main/java/com/swkang/playground/view/covid19/Covid19StatusListAdapter.kt
@@ -14,14 +14,14 @@ import com.swkang.playground.R
 
 class Covid19StatusListAdapter(
     private val vm: Covid19StatusViewModel
-) : ListAdapter<LocationCovid19Infos, Covid19StatusListAdapter.Covid19StatusViewHolder>(difUtil) {
+) : ListAdapter<LocationCovid19Infos, RecyclerView.ViewHolder>(diffUtil) {
 
     // Diff util
     companion object {
         const val VIEWTYPE_TOP = 0
         const val VIEWTYPE_DEFAULT = 1
 
-        val difUtil = object : DiffUtil.ItemCallback<LocationCovid19Infos>() {
+        val diffUtil = object : DiffUtil.ItemCallback<LocationCovid19Infos>() {
             override fun areItemsTheSame(
                 oldItem: LocationCovid19Infos,
                 newItem: LocationCovid19Infos
@@ -36,12 +36,16 @@ class Covid19StatusListAdapter(
                 return oldItem == newItem
             }
         }
+    }// end of DiffUtil class
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEWTYPE_TOP -> Covid19StatusTopViewHolder.create(parent)
+            else -> Civud18StatusCountryViewHolder.create(parent)
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Covid19StatusViewHolder =
-        Covid19StatusViewHolder.create(parent, viewType)
-
-    override fun onBindViewHolder(holder: Covid19StatusViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is Covid19StatusTopViewHolder) holder.bind(vm)
         else if (holder is Civud18StatusCountryViewHolder) holder.bind(getItem(position))
     }
@@ -53,40 +57,28 @@ class Covid19StatusListAdapter(
         }
 
     /**
-     * 뷰타입에 따른 각 뷰홀더의 부모 클래스
-     */
-    abstract class Covid19StatusViewHolder(
-        binding: ViewDataBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-        companion object {
-            fun create(parent: ViewGroup, viewType: Int): Covid19StatusViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val viewResId = when (viewType) {
-                    VIEWTYPE_TOP -> R.layout.covid19status_item_top
-                    VIEWTYPE_DEFAULT -> R.layout.covid19status_item_country
-                    else -> throw IllegalArgumentException("`$viewType` is not available view type integer.")
-                }
-                val binding = DataBindingUtil.inflate<ViewDataBinding>(
-                    layoutInflater,
-                    viewResId,
-                    parent,
-                    false
-                )
-                return if (viewType == VIEWTYPE_TOP) Covid19StatusTopViewHolder(binding)
-                else Civud18StatusCountryViewHolder(binding)
-            }
-        }
-    }
-
-    /**
      * 상단 헤더 뷰 홀더
      *
      * - 국내/세계 감염자, 사망자, 완치자 현황
      * - 시-도/각 국가 별 현황 리스트 목록의 헤더
      * - 목록에서 0번째 포지션에 무조건 한개만 존재
      */
-    class Covid19StatusTopViewHolder(private val binding: ViewDataBinding) :
-        Covid19StatusViewHolder(binding) {
+    class Covid19StatusTopViewHolder(
+        private val binding: ViewDataBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun create(parent: ViewGroup): Covid19StatusTopViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = DataBindingUtil.inflate<ViewDataBinding>(
+                    layoutInflater,
+                    R.layout.covid19status_item_top,
+                    parent,
+                    false
+                )
+                return Covid19StatusTopViewHolder(binding)
+            }
+        }
+
         fun bind(vm: Covid19StatusViewModel) {
             binding.setVariable(BR.vm, vm)
             binding.executePendingBindings()
@@ -96,8 +88,22 @@ class Covid19StatusListAdapter(
     /**
      * 시-도/각 국가 별 현황 리스트 아이템 항목에 대한 뷰 홀더
      */
-    class Civud18StatusCountryViewHolder(private val binding: ViewDataBinding) :
-        Covid19StatusViewHolder(binding) {
+    class Civud18StatusCountryViewHolder(
+        private val binding: ViewDataBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun create(parent: ViewGroup): Civud18StatusCountryViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = DataBindingUtil.inflate<ViewDataBinding>(
+                    layoutInflater,
+                    R.layout.covid19status_item_country,
+                    parent,
+                    false
+                )
+                return Civud18StatusCountryViewHolder(binding)
+            }
+        }
+
         fun bind(covid19Location: LocationCovid19Infos) {
             binding.setVariable(BR.covid19Location, covid19Location)
             binding.executePendingBindings()
