75b6e3dec8ca28748aefde0f0d1c1de65d3bf9a0
beemo <burkdog@naver.com>
2021-03-04 21:11:10 +0900

    [추가] Contract 테스트 액티비티, 프래그먼트 추가 중

---
+++ app/src/main/java/com/swkang/playground/view/contracts/StartContractsFragment.kt
@@ -0,0 +1,11 @@
+package com.swkang.playground.view.contracts
+
+import com.swkang.model.base.BaseViewModel
+import com.swkang.playground.R
+import com.swkang.playground.base.BaseFragment
+
+class StartContractsFragment: BaseFragment() {
+    override fun getLayoutId(): Int = R.layout.contracts_start_fragment
+
+    override fun createViewModel(): BaseViewModel =
+}
